65359e2301e5ce2cc8ac032a23cf787ec6ccaa4b
Gayathri <gayathrimh11@gmail.com>
2025-12-23 19:22:52 +0530

    Convert products into classes

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -1,14 +1,6 @@
-import {cart,removeFromCart,updateDeliveryOption} from '../data/cart.js';
-import {products} from '../data/products.js';
-import {formatCurrency} from './utils/money.js';
-import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
-import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import {deliveryOptions} from '../data/deliveryOptions.js';
-import '../data/cart-class.js';
-import ''
-
-hello();
-
+import {renderOrderSummary} from './checkout/orderSummary.js';
+import {renderPaymentSummary} from './checlout/paymentSummary.js';
+//import '../data/cart-class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
